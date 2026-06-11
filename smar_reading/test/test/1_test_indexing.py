5d3b401f62e5dd7864207a2c3078de777f98c87f
李治典 <17158151+dianzhi-li@user.noreply.gitee.com>
2026-06-12 01:38:24 +0800

    索引构建阶段测试代码

---
+++ smar_reading/test/test/1_test_indexing.py
@@ -0,0 +1,10 @@
+from config.setting import QaConfig
+from indexing.indexing_pipeline import IndexingPipeline
+
+cfg = QaConfig()
+pipeline = IndexingPipeline(cfg)
+
+# 从文件构建
+pdf_file_path = "D:\Workspace\PycharmProjects\PythonProject\k-ai-knowledge\smar_reading\data\sample_document .pdf"
+file_hash = pipeline.build_from_file(pdf_file_path, "sk-e6a85c002d804560a7ef3d332d0075e9")
+print(f"索引构建完成，file_hash: {file_hash}")
